01d6e4e1bf2178e98f09527bfd7ae53d3353b051
Erazmus1999 <erazmus1999@gmail.com>
2022-10-17 03:25:21 +0200

    Add project files.

---
+++ PasswordWalletMVC/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using PasswordWalletMVC.Models;
+using System.Linq;
+
+
+namespace PasswordWalletMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserAccount account)
+        {
+            if(ModelState.IsValid)
+            {
+                using(OurDbContext db = new OurDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                // We will be displaying hashed password there
+                ViewBag.Message = account.FirstName + " " + account.LastName + "Successfully registered.";
+            }   
+            return View();
+        }
+
+        //Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserAccount user)
+        {
+            using(OurDbContext db = new OurDbContext())
+            {
+               
+                var usr = db.userAccount.Single(u => u.UserName == user.UserName && u.Password == user.Password);
+
+                
+                if(usr != null)
+                {
+                    
+                    HttpContext.Session.SetString("UserId"  , usr.UserId.ToString());
+                    HttpContext.Session.SetString("UserName", usr.UserId.ToString());
+
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "UserName or Password is wrong.");
+                }
+                return View();
+            }
+
+            //public ActionResult
+        }
+    }
+}
---
+++ PasswordWalletMVC/Models/UserAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PasswordWalletMVC.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="First Name required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [RegularExpression(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "UserName required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
